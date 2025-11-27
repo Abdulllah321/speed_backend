e8e6ba671d9806a17bf80db868e02bbbdc340374
Abdulllah321 <abdullahsufyan2007@gmail.com>
2025-11-27 19:58:19 +0500

    Integrate branchRoutes into the main router for API access

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,8 +6,8 @@ import maritalStatusRoutes from "./maritalStatusRoutes.js";
 import taxSlabRoutes from "./taxSlabRoutes.js";
 import eobiRoutes from "./eobiRoutes.js";
 import authRoutes from "./authRoutes.js";
-import designationRoutes from "./designationRoutes.js";
 import cityRoutes from "./cityRoutes.js";
+import branchRoutes from "./branchRoutes.js";
 const router = express.Router();
 
 // API routes
@@ -20,4 +20,5 @@ router.use("/api", taxSlabRoutes);
 router.use("/api", eobiRoutes);
 router.use("/api", designationRoutes);
 router.use("/api", cityRoutes);
+router.use("/api", branchRoutes);
 export default router;
